43f3e551e6260193788fe3b06e860a8808fe54d3
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2019-09-26 23:46:15 +0200

    feat: remove json body support in go templates
    It's not useful anymore thanks to yaml

--- templates/go_template.go
+++ templates/go_template.go
@@ -2,7 +2,6 @@ package templates
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"text/template"
 
@@ -30,30 +29,8 @@ func (*goTemplateEngine) Execute(request types.Request, script string) (*types.M
 		return nil, fmt.Errorf("failed to execute dynamic template: %w", err)
 	}
 
-	var tmplResult map[string]interface{}
-	if err = yaml.Unmarshal(buffer.Bytes(), &tmplResult); err != nil {
-		log.WithError(err).Error("Failed to unmarshal response from dynamic template")
-		return nil, fmt.Errorf("failed to unmarshal response from dynamic template: %w", err)
-	}
-
-	body := tmplResult["body"]
-	if _, ok := body.(string); !ok {
-		// FIXME: this should depend on the Content-Type of the tmplResult
-		b, err := json.Marshal(body)
-		if err != nil {
-			log.WithError(err).Error("Failed to marshal response body as JSON")
-			return nil, fmt.Errorf("failed to marshal response body as JSON: %w", err)
-		}
-		tmplResult["body"] = string(b)
-	}
-
-	b, err := yaml.Marshal(tmplResult)
-	if err != nil {
-		log.WithError(err).Error("Failed to marshal template result as YAML")
-	}
-
 	var result types.MockResponse
-	if err = yaml.Unmarshal(b, &result); err != nil {
+	if err = yaml.Unmarshal(buffer.Bytes(), &result); err != nil {
 		log.WithError(err).Error("Failed to unmarshal response as mock response")
 	}
 
