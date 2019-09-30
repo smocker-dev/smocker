575e3c95fcb1bc9399c7e73d9cccfb65a12fed53
Gwendal Leclerc <gwendal.leclerc@gmail.com>
2019-09-30 02:38:51 +0200

    feat: add some navbar and init navigation

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,17 +1,22 @@
 port module Main exposing (Model, Msg(..), init, main, update, view)
 
-import Browser
+import Browser exposing (Document)
+import Browser.Navigation as Navigation
+import Header
 import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, style)
 import Html.Events exposing (onClick)
+import Url exposing (Url)
 
 
 port title : String -> Cmd msg
 
 
 main =
-    Browser.element
+    Browser.application
         { init = init
+        , onUrlChange = ChangedUrl
+        , onUrlRequest = ClickedLink
         , update = update
         , subscriptions = subscriptions
         , view = view
@@ -29,16 +34,14 @@ type alias Flags =
 
 
 type alias Model =
-    { basePath : String
-    , version : String
+    { flags : Flags
     , counter : Int
     }
 
 
-init : Flags -> ( Model, Cmd Msg )
-init flags =
-    ( { basePath = flags.basePath
-      , version = flags.version
+init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags _ _ =
+    ( { flags = flags
       , counter = 0
       }
     , title flags.version
@@ -50,22 +53,15 @@ init flags =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = ChangedUrl Url
+    | ClickedLink Browser.UrlRequest
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | counter = model.counter + 1 }
-            , Cmd.none
-            )
-
-        Decrement ->
-            ( { model | counter = model.counter - 1 }
-            , Cmd.none
-            )
+        _ ->
+            ( model, Cmd.none )
 
 
 
@@ -81,10 +77,13 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Document Msg
 view model =
-    div [ class "buttons has-addons is-centered" ]
-        [ button [ class "button is-primary", onClick Decrement ] [ text "-" ]
-        , button [ class "button is-static", onClick Increment ] [ text (String.fromInt model.counter) ]
-        , button [ class "button is-primary", onClick Increment ] [ text "+" ]
+    { title = "Home"
+    , body =
+        [ div [ class "has-background-black-ter", style "height" "100%" ]
+            [ Header.view
+            , div [ class "section has-text-centered has-text-white" ] [ text "Under construction" ]
+            ]
         ]
+    }
