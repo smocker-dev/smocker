a09af4152c35d34ae17e26c01e278619e946fc76
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2020-01-28 23:37:27 +0100

    add favicons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,55 @@ const path = require("path");
 
 module.exports = {
   title: "Smocker",
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/logo/favicon/apple-touch-icon.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/logo/favicon/favicon-32x32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/logo/favicon/favicon-16x16.png"
+      }
+    ],
+    ["link", { rel: "manifest", href: "/logo/favicon/site.webmanifest" }],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/logo/favicon/safari-pinned-tab.svg",
+        color: "#b51629"
+      }
+    ],
+    ["link", { rel: "shortcut icon", href: "/logo/favicon/favicon.ico" }],
+    ["meta", { name: "apple-mobile-web-app-title", content: "Smocker" }],
+    ["meta", { name: "application-name", content: "Smocker" }],
+    ["meta", { name: "msapplication-TileColor", content: "#446fcd" }],
+    [
+      "meta",
+      {
+        name: "msapplication-config",
+        content: "/logo/favicon/browserconfig.xml"
+      }
+    ],
+    ["meta", { name: "theme-color", content: "#ffffff" }]
+  ],
   chainWebpack: (config, _) => {
     config.resolve.alias.set("@root", path.resolve(__dirname, ".."));
   },
@@ -25,7 +74,7 @@ module.exports = {
         'div[class*="language-"] pre',
         'div[class*="aside-code"] aside'
       ],
-      copyMessage: "Copy successfully and then paste it for use.",
+      copyMessage: "Code copied to your clipboard.",
       duration: 300
     }
   ]
