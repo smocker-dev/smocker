872332b8cc1ca14b485890aa8040664dd051babc
Gwendal Leclerc <gwendal.leclerc@gmail.com>
2020-02-10 22:15:17 +0100

    fix: parcel config for building antd components

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 import App from "~components/App";
-import "antd/dist/antd.css";
 
 render(<App />, document.getElementById("root"));
 
