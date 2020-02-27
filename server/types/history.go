3825796ed74df576d358b3facd178007fd32177d
Gwendal Leclerc <gwendal.leclerc@gmail.com>
2020-02-27 02:00:22 +0100

    feat: add origin to request structure using headers and RemoteAddr

--- server/types/history.go
+++ server/types/history.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -32,8 +34,9 @@ type Entry struct {
 type Request struct {
 	Path        string      `json:"path"`
 	Method      string      `json:"method"`
-	Body        interface{} `json:"body,omitempty" yaml:"body,omitempty"`
+	Origin      string      `json:"origin"`
 	BodyString  string      `json:"-" yaml:"-"`
+	Body        interface{} `json:"body,omitempty" yaml:"body,omitempty"`
 	QueryParams url.Values  `json:"query_params,omitempty" yaml:"query_params,omitempty"`
 	Headers     http.Header `json:"headers,omitempty" yaml:"headers,omitempty"`
 	Date        time.Time   `json:"date" yaml:"date"`
@@ -72,14 +75,36 @@ func HTTPRequestToRequest(req *http.Request) Request {
 		}
 	}
 	headers.Add("Host", req.Host)
-
 	return Request{
 		Path:        req.URL.Path,
 		Method:      req.Method,
+		Origin:      getOrigin(req),
 		Body:        body,
 		BodyString:  string(bodyBytes),
 		QueryParams: req.URL.Query(),
 		Headers:     headers,
 		Date:        time.Now(),
 	}
+}
+
+func getOrigin(r *http.Request) string {
+	for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+		addresses := strings.Split(r.Header.Get(h), ",")
+		// march from right to left until we get a public address
+		// that will be the address right before our proxy.
+		for i := len(addresses) - 1; i >= 0; i-- {
+			// header can contain spaces too, strip those out.
+			ip := strings.TrimSpace(addresses[i])
+			if ip != "" {
+				return ip
+			}
+		}
+	}
+
+	// port declared in RemoteAddr is inconsistent so we can't use it
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+	return host
 }
