b8604ecd957dc9d5c415f5f561186c57714b7fd1
Thibaut Rousseau <contact@thibaut-rousseau.com>
2020-05-23 16:05:17 +0200

    add describe scope to jest tests

--- client/src/modules/utils.test.ts
+++ client/src/modules/utils.test.ts
@@ -19,96 +19,100 @@ const baseEntry: Entry = {
   },
 };
 
-test("GET history entry", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "GET",
-      path: "/test",
-    },
-  };
-  expect(entryToCurl(entry)).toBe("curl -XGET '/test'");
-});
-
-test("GET history entry with Host header", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "GET",
-      path: "/test",
-      headers: {
-        Host: ["localhost:8080"],
+describe("Generate curl command from:", () => {
+  test("GET history entry", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "GET",
+        path: "/test",
       },
-    },
-  };
-  expect(entryToCurl(entry)).toBe("curl -XGET 'localhost:8080/test'");
-});
+    };
+    expect(entryToCurl(entry)).toBe("curl -XGET '/test'");
+  });
 
-test("GET history entry with headers", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "GET",
-      path: "/test",
-      headers: {
-        "X-Foo-Header": ["foo"],
-        "X-Bar-Header": ["bar", "baz"],
+  test("GET history entry with Host header", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "GET",
+        path: "/test",
+        headers: {
+          Host: ["localhost:8080"],
+        },
       },
-    },
-  };
-  expect(entryToCurl(entry)).toBe(
-    "curl -XGET --header 'X-Foo-Header: foo' --header 'X-Bar-Header: bar' --header 'X-Bar-Header: baz' '/test'"
-  );
-});
+    };
+    expect(entryToCurl(entry)).toBe("curl -XGET 'localhost:8080/test'");
+  });
 
-test("GET history entry with query parameters", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "GET",
-      path: "/test",
-      query_params: {
-        foo: ["foo"],
-        bar: ["bar", "baz"],
+  test("GET history entry with headers", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "GET",
+        path: "/test",
+        headers: {
+          "X-Foo-Header": ["foo"],
+          "X-Bar-Header": ["bar", "baz"],
+        },
       },
-    },
-  };
-  expect(entryToCurl(entry)).toBe("curl -XGET '/test?foo=foo&bar=bar&bar=baz'");
-});
+    };
+    expect(entryToCurl(entry)).toBe(
+      "curl -XGET --header 'X-Foo-Header: foo' --header 'X-Bar-Header: bar' --header 'X-Bar-Header: baz' '/test'"
+    );
+  });
 
-test("GET history entry with body JSON", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "POST",
-      path: "/test",
-      body: {
-        key: "value containing 'single quotes'",
+  test("GET history entry with query parameters", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "GET",
+        path: "/test",
+        query_params: {
+          foo: ["foo"],
+          bar: ["bar", "baz"],
+        },
       },
-    },
-  };
-  expect(entryToCurl(entry)).toBe(
-    `curl -XPOST '/test' --data '{"key":"value containing \\'single quotes\\'"}'`
-  );
-});
+    };
+    expect(entryToCurl(entry)).toBe(
+      "curl -XGET '/test?foo=foo&bar=bar&bar=baz'"
+    );
+  });
 
-test("GET history entry with body string", () => {
-  const entry: Entry = {
-    ...baseEntry,
-    request: {
-      ...baseEntry.request,
-      method: "POST",
-      path: "/test",
-      body: "value containing 'single quotes'",
-    },
-  };
-  expect(entryToCurl(entry)).toBe(
-    // FIXME: we shouldn't have the " if the client sent raw text
-    `curl -XPOST '/test' --data '"value containing \\'single quotes\\'"'`
-  );
+  test("GET history entry with body JSON", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "POST",
+        path: "/test",
+        body: {
+          key: "value containing 'single quotes'",
+        },
+      },
+    };
+    expect(entryToCurl(entry)).toBe(
+      `curl -XPOST '/test' --data '{"key":"value containing \\'single quotes\\'"}'`
+    );
+  });
+
+  test("GET history entry with body string", () => {
+    const entry: Entry = {
+      ...baseEntry,
+      request: {
+        ...baseEntry.request,
+        method: "POST",
+        path: "/test",
+        body: "value containing 'single quotes'",
+      },
+    };
+    expect(entryToCurl(entry)).toBe(
+      // FIXME: we shouldn't have the " if the client sent raw text
+      `curl -XPOST '/test' --data '"value containing \\'single quotes\\'"'`
+    );
+  });
 });
