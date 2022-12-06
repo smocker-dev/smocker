c73afe707275a17ad91486b450db597b21ba2627
Gwendal Leclerc <gwendal.leclerc@corp.ovh.com>
2022-12-06 22:29:53 +0100

    feat: use accordion when text is too long on editor

--- client/components/Code.tsx
+++ client/components/Code.tsx
@@ -1,4 +1,12 @@
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Spinner
+} from "@chakra-ui/react";
 import Editor, { OnMount } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import React from "react";
@@ -7,17 +15,33 @@ const largeBodyLength = 5000;
 
 export type Language = "yaml" | "json" | "lua" | "xml" | "txt";
 
-export const Code = ({
+const readOnlyOptions: editor.IStandaloneEditorConstructionOptions = {
+  minimap: {
+    enabled: false // disable minimap
+  },
+  wordWrap: "on", // do not scroll on x
+  scrollBeyondLastLine: false, // do not authorize to scroll over height
+  scrollbar: {
+    alwaysConsumeMouseWheel: false, // do not catch scroll event on page
+    vertical: "hidden", // hidde vertical scrollbar
+    horizontal: "hidden" // hidde horizontal scrollbar
+  },
+  renderLineHighlight: "none", // do not focus on selected line
+  contextmenu: false, // do not display contextual menu on right click
+  overviewRulerLanes: 0, // disable overview on the right
+  readOnly: true,
+  lineHeight: 12
+};
+
+const EditorBox = ({
   value,
   language,
-  onChange,
-  collapsible = true
+  onChange
 }: {
   value?: string;
   language: Language;
-  collapsible?: boolean;
   onChange?: (value: string) => unknown;
-}): JSX.Element => {
+}) => {
   const [height, setHeight] = React.useState(20);
 
   const updateHeight = (editor: editor.IStandaloneCodeEditor) => {
@@ -38,24 +62,48 @@ export const Code = ({
         value={value}
         language={language}
         theme="vs-dark"
-        options={{
-          minimap: {
-            enabled: false // disable minimap
-          },
-          wordWrap: "on", // do not scroll on x
-          scrollBeyondLastLine: false, // do not authorize to scroll over height
-          scrollbar: {
-            alwaysConsumeMouseWheel: false, // do not catch scroll event on page
-            vertical: "hidden", // hidde vertical scrollbar
-            horizontal: "hidden" // hidde horizontal scrollbar
-          },
-          renderLineHighlight: "none", // do not focus on selected line
-          contextmenu: false, // do not display contextual menu on right click
-          overviewRulerLanes: 0, // disable overview on the right
-          readOnly: true,
-          lineHeight: 12
-        }}
+        options={readOnlyOptions}
+        onChange={onChange ? v => onChange(v || "") : undefined}
       />
     </Box>
   );
+};
+
+export const Code = ({
+  value,
+  language,
+  onChange,
+  collapsible = true
+}: {
+  value?: string;
+  language: Language;
+  collapsible?: boolean;
+  onChange?: (value: string) => unknown;
+}): JSX.Element => {
+  if (collapsible && value && value.length > largeBodyLength) {
+    return (
+      <Accordion allowToggle reduceMotion>
+        <AccordionItem borderWidth="1px" borderColor="sidebar.border">
+          {({ isExpanded }) => (
+            <>
+              <AccordionButton bgColor="grey.light">
+                <Box flex="1" textAlign="left">
+                  This payload is huge. Click to display it
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel>
+                <EditorBox
+                  value={value}
+                  language={language}
+                  onChange={onChange}
+                />
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      </Accordion>
+    );
+  }
+  return <EditorBox value={value} language={language} onChange={onChange} />;
 };
